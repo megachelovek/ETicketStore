6b78640266ac86b3cd1ff43e0bcaa6c37f98cab4
Данила Мальчиков <2regvk@mail.ru>
2024-05-05 15:13:32 +0400

    feat: Project creation

---
+++ ETicketStore.Common/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ETicketStore.Common.Models
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ ETicketStore.Common/Models/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using ETicketStore.Common.Models;
+using System;
+
+namespace ETicketStore.Common
+{
+    public class Ticket
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime EventDateTime { get; set;}
+        public string Place { get; set; }
+        public Customer Customer { get; set; }
+        public decimal Price { get; set; }
+        public string Address { get; set; }
+    }
+}
